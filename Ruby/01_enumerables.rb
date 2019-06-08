994aa5691b5798c84a2949e3c61eeb2918318b7d
Kalib Hackin <nagui08@hotmail.com>
2019-06-08 13:25:20 -0500

    my any done

--- Ruby/01_enumerables.rb
+++ Ruby/01_enumerables.rb
@@ -31,14 +31,26 @@ module Enumerable
     count = 0
     aux = []
     while count < length
-      aux << self[count] if !yield(self[count])
+      aux << self[count] unless yield(self[count])
       count += 1
     end
     aux
+  end
+
+  def my_any?
+    return true unless block_given?
+
+    count = 0
+    while count < length
+      return true if yield(self[count])
+
+      count += 1
+    end
+    false
   end
 end
 
 arr = [1, 2, 3, 4, 5]
 
-p arr.reject { |x| x < 3 }
-p arr.my_reject { |x| x < 3 }
+p arr.any? { |x| x > 3 }
+p arr.my_any? { |x| x > 3 }
