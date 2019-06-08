133adf55ed4146b1f852bd5c93f95be1c2786beb
Kalib Hackin <nagui08@hotmail.com>
2019-06-08 13:11:11 -0500

    fixing the rubocop issues

--- Ruby/01_enumerables.rb
+++ Ruby/01_enumerables.rb
@@ -1,28 +1,33 @@
-module Enumerable
-    def my_each
-        return self unless block_given?
-        count = 0
-        while count < self.length
-            yield(self[count])
-            count += 1
-        end
-        self
-    end
+# frozen_string_literal: true
 
-    def my_select
-        return self unless block_given?
-        count = 0
-        aux = []
-        while count < self.length
-            aux << self[count] if yield(self[count])
-            count += 1
-        end
-        aux
+# simulating the enumerable methods
+module Enumerable
+  def my_each
+    return self unless block_given?
+
+    count = 0
+    while count < length
+      yield(self[count])
+      count += 1
     end
+    self
+  end
+
+  def my_select
+    return self unless block_given?
+
+    count = 0
+    aux = []
+    while count < length
+      aux << self[count] if yield(self[count])
+      count += 1
+    end
+    aux
+  end
 end
 
 arr = [1, 2, 3, 4, 5]
 
 arr.select { |x| p x > 2 }
-p "--- this is a line ---"
+p '--- this is a line ---'
 arr.my_select { |x| p x > 2 }
