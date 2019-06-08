57819823623a5e117757f52da0d42188dbe5bf49
Kalib Hackin <nagui08@hotmail.com>
2019-06-08 13:15:30 -0500

    my reject done

--- Ruby/01_enumerables.rb
+++ Ruby/01_enumerables.rb
@@ -24,10 +24,21 @@ module Enumerable
     end
     aux
   end
+
+  def my_reject
+    return self unless block_given?
+
+    count = 0
+    aux = []
+    while count < length
+      aux << self[count] if !yield(self[count])
+      count += 1
+    end
+    aux
+  end
 end
 
 arr = [1, 2, 3, 4, 5]
 
-arr.select { |x| p x > 2 }
-p '--- this is a line ---'
-arr.my_select { |x| p x > 2 }
+p arr.reject { |x| x < 3 }
+p arr.my_reject { |x| x < 3 }
